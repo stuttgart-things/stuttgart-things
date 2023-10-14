6abd08f1f2a4446f400cc3817286d23730eacd99
Patrick Hermann <patrick.hermann@sva.de>
2023-10-14 15:44:23 +0000

    disabled monitoring, changed secret name for msk

--- stageTime/run-status.sh
+++ stageTime/run-status.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+sleep=10
 failed_prs=0
 succeeded_prs=0
+retries=0
+max_retries=3
 
 all_prs=$(tkn pr list -n tektoncd | grep -c alpine)
 echo all pipelineRuns: ${all_prs}
 
-while [[ ${failed_prs} -ge 0  ]] || [[ ${succeeded_prs} -eq ${all_prs} ]]
+while [[ ${failed_prs} -le 0  ]] || [[ ${succeeded_prs} -eq ${all_prs} ]] || [[ ${retries} -eq ${max_retries} ]]
 do
+    echo ${retries_left} retries left
+    echo check/retry in ${sleep} seconds..
+    sleep ${sleep}
+
     failed_prs=$(tkn pr list -n tektoncd | grep alpine | grep -c Failed)
     echo Failed pipelineRuns: ${failed_prs}
     tkn pr list -n tektoncd | grep alpine | grep Failed
@@ -15,7 +22,8 @@ do
     echo Succeeded pipelineRuns: ${succeeded_prs}
     tkn pr list -n tektoncd | grep alpine | grep Succeeded
 
-    sleep 5
+    retries=`expr ${retries} + 1`
+    retries_left=`expr ${max_retries} - ${retries}`
 done
 
 echo "Done watching pipelineRuns!"
