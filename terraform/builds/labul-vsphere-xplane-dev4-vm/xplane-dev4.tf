5eefdc1bdffb9e1841cca84617d905c2232f04d7
patrick-hermann-sva <47567770+patrick-hermann-sva@users.noreply.github.com>
2024-06-07 04:38:12 +0000

    [CI][terraform] - Created template config for vm xplane-dev4 ubuntu24-labul-vsphere

---
+++ terraform/builds/labul-vsphere-xplane-dev4-vm/xplane-dev4.tf
@@ -0,0 +1,56 @@
+module "xplane-dev4" {
+  source                 = "github.com/stuttgart-things/vsphere-vm?ref=v1.7.5-2.7.0-1"
+  vm_count               = 1
+  vsphere_vm_name        = "xplane-dev4"
+  vm_memory              = 12288
+  vsphere_vm_template    = "sthings-u24"
+  vm_disk_size           = "128"
+  vm_num_cpus            = 8
+  firmware               = "bios"
+  vsphere_vm_folder_path = "stuttgart-things/testing"
+  vsphere_datacenter     = "/LabUL"
+  vsphere_datastore      = "/LabUL/datastore/UL-ESX-SAS-01"
+  vsphere_resource_pool  = "/LabUL/host/Cluster-V6.5/Resources"
+  vsphere_network        = "/LabUL/network/MGMT-10.31.101"
+  bootstrap              = ["echo STUTTGART-THINGS"]
+  annotation             = "VSPHERE-VM xplane-dev4 sthings-u24 BUILD w/ TERRAFORM FOR STUTTGART-THINGS"
+  vsphere_server         = var.vsphere_server
+  vsphere_user           = var.vsphere_user
+  vsphere_password       = var.vsphere_password
+  vm_ssh_user            = var.vm_ssh_user
+  vm_ssh_password        = var.vm_ssh_password
+}
+
+variable "vsphere_server" {
+  default     = false
+  type        = string
+  description = "name of vsphere vm server"
+}
+
+variable "vm_ssh_user" {
+  default     = false
+  type        = string
+  description = "username of ssh user for vm"
+}
+
+variable "vm_ssh_password" {
+  default     = false
+  type        = string
+  description = "password of ssh user"
+  }
+
+variable "vsphere_user" {
+  default     = false
+  type        = string
+  description = "password of vsphere user"
+}
+
+variable "vsphere_password" {
+  default     = false
+  type        = string
+  description = "password of vsphere user"
+}
+
+output "ip" {
+  value = [module.xplane-dev4.ip]
+}
