c0456964c42a918bef08af576016d7b3f8f86ac5
Patrick Hermann <patrick.hermann@sva.de>
2024-06-25 15:47:39 +0000

    added zot

--- images/get.sh
+++ images/get.sh
@@ -9,10 +9,6 @@ for registry in $(yq eval -o=j ${profile} | jq -cr '.registries[]'); do
   vault_path_username=$(echo $registry | jq -r '.username' -)
   vault_path_password=$(echo $registry | jq -r '.password' -)
 
-  machineshop get --path ${vault_path_username} | tail -n +8
-  machineshop get --path ${vault_path_password} | tail -n +8
-
-
-  echo $alias $url $vault_path_username $vault_path_password
+  skopeo login $url --username $(machineshop get --path ${vault_path_username} | tail -n +8) --password $(machineshop get --path ${vault_path_password} | tail -n +8)
 
 done
